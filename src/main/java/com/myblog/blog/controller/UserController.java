17f7b02d361711a8beced0d6c658fbdf7b868aef
PanPurpura <jarek.such0@gmail.com>
2024-03-15 16:37:45 +0100

    Added three new methods.

--- src/main/java/com/myblog/blog/controller/UserController.java
+++ src/main/java/com/myblog/blog/controller/UserController.java
@@ -2,9 +2,7 @@ package com.myblog.blog.controller;
 
 import com.myblog.blog.model.User;
 import com.myblog.blog.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +16,43 @@ public class UserController {
         this.repository = repository;
     }
 
+    public record Credentials(String email, String password) {};
+
     @GetMapping("/all")
     public List<User> getAll() {
-        System.out.println("SIUU");
         return (List<User>) repository.findAll();
+    }
+
+    @PostMapping("/getLogged")
+    public String getLogged(@RequestBody Credentials cred) {
+        Credentials c = new Credentials(cred.email(), cred.password());
+        User logged = repository.getUserByEmailAAndPassword(c.email(), c.password());
+
+        if(logged == null)
+            return "Wrong email or password";
+        else
+            return "Successfully logged";
+
+    }
+    @PostMapping("/registration")
+    public String registration(@RequestBody User user) {
+
+        if(repository.getUserByEmail(user.getEmail()) != null) {
+            return "Email already taken";
+        }
+        else if (repository.getUserByUsername(user.getUsername()) != null) {
+            return  "Username already taken";
+        }
+        else {
+            User u = new User();
+            u.setUsername(user.getUsername());
+            u.setEmail(user.getEmail());
+            u.setPassword(user.getPassword());
+            repository.save(u);
+            return "Account successfully created!";
+        }
+
+
     }
 
 }
