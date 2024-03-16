2c727a84a294bea21b1b7b696173197b41dcc647
PanPurpura <jarek.such0@gmail.com>
2024-03-16 23:15:24 +0100

    Added three new methods.

--- src/main/java/com/myblog/blog/repository/UserRepository.java
+++ src/main/java/com/myblog/blog/repository/UserRepository.java
@@ -2,9 +2,21 @@ package com.myblog.blog.repository;
 
 import com.myblog.blog.model.User;
 import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM users u WHERE u.email = :email AND u.password = :password")
+    public User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM users u WHERE u.email = :email")
+    public User findByEmail(@Param("email") String email);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM users u WHERE u.username = :username")
+    public User findByUsername(@Param("username") String username);
+
 }
