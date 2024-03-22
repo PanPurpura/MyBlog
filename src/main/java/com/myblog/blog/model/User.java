1dcadb37c60a02144068f9ef58d4f4c08f657db7
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-03-22 15:00:10 +0100

    Extend class.

--- src/main/java/com/myblog/blog/model/User.java
+++ src/main/java/com/myblog/blog/model/User.java
@@ -1,5 +1,6 @@
 package com.myblog.blog.model;
 
+import com.myblog.blog.Services.UserService;
 import jakarta.persistence.*;
 
 import java.util.Objects;
@@ -17,12 +18,23 @@ public class User {
     private String password;
     @Column(nullable = false)
     private String  email;
+    private String name;
+    private String surname;
+    @Column(nullable = false)
+    private Boolean active;
+    @Column(nullable = false)
+    private String role;
 
-    public User(Integer id, String username, String password, String email) {
+    @Column(nullable = false)
+    private Boolean locked;
+
+    public User(Integer id, String username, String password, String email, String name, String surname) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
+        this.name = name;
+        this.surname = surname;
     }
 
     public User() {
@@ -60,17 +72,57 @@ public class User {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(email, user.email) && Objects.equals(name, user.name) && Objects.equals(surname, user.surname) && Objects.equals(active, user.active) && Objects.equals(role, user.role) && Objects.equals(locked, user.locked);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, password, email);
+        return Objects.hash(id, username, password, email, name, surname, active, role, locked);
     }
 
     @Override
@@ -80,6 +132,11 @@ public class User {
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", active=" + active +
+                ", role='" + role + '\'' +
+                ", locked=" + locked +
                 '}';
     }
 }
