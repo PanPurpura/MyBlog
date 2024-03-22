71dc5628dbd924cbc10a13f0fc3b6c2cde7b23ed
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-03-22 15:00:49 +0100

    User DTO pattern class.

---
+++ src/main/java/com/myblog/blog/DTO/UserDTO.java
@@ -0,0 +1,75 @@
+package com.myblog.blog.DTO;
+
+import java.util.Objects;
+
+public class UserDTO {
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+
+    public UserDTO(String username, String name, String surname, String email) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public UserDTO() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return Objects.equals(username, userDTO.username) && Objects.equals(name, userDTO.name) && Objects.equals(surname, userDTO.surname) && Objects.equals(email, userDTO.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, name, surname, email);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
