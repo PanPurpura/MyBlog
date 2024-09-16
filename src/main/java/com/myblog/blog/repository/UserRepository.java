4d1d80b0c36de9094666119dc414da0e4a42a22b
PanPurpura <jarek.such0@gmail.com>
2024-09-16 21:57:13 +0200

    Update: Changing return types.

--- src/main/java/com/myblog/blog/repository/UserRepository.java
+++ src/main/java/com/myblog/blog/repository/UserRepository.java
@@ -8,17 +8,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     public User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
-    public User findByEmail(@Param("email") String email);
+    public Optional<User> findByEmail(@Param("email") String email);
 
-    public User findByUsername(@Param("username") String username);
-
-    public List<User> findAllByActive(Boolean active);
+    public Optional<User> findByLogin(@Param("username") String username);
 
     public User findById(long id);
 
