fb5df71bb1b5dd91f7d20c7b8f5754021589690e
PanPurpura <jarek.such0@gmail.com>
2024-09-23 13:24:16 +0200

    Updated: added lombom annotation and relations

--- src/main/java/com/myblog/blog/model/BlogPost.java
+++ src/main/java/com/myblog/blog/model/BlogPost.java
@@ -2,130 +2,46 @@ package com.myblog.blog.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.awt.image.BufferedImage;
 import java.util.Objects;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "blogposts")
+@Table(name = "blog_posts")
 public class BlogPost {
 
     @Id
     @GeneratedValue
     private Integer Id;
-
-    @OneToOne
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
     @Column(nullable = false)
     private String title;
     @Column(nullable = false)
     private String text;
 
-    @ElementCollection
-    private List<String> comments;
+    @OneToMany(mappedBy = "blogPost")
+    private List<Comment> commentList;
 
-    private byte[] picture;
+    @OneToMany(mappedBy = "blogPost")
+    private List<BlogPostImage> blogPostImageList;
 
     @Column(nullable = false)
-    private Date date;
+    private LocalDateTime addedAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
-    public BlogPost(Integer id, User user, String title, String text, List<String> comments, byte[] picture, Date date) {
-        Id = id;
-        this.user = user;
-        this.title = title;
-        this.text = text;
-        this.comments = comments;
-        this.picture = picture;
-        this.date = date;
-    }
-
-    public BlogPost() {
-    }
-
-    public Integer getId() {
-        return Id;
-    }
-
-    public void setId(Integer id) {
-        Id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public List<String> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<String> comments) {
-        this.comments = comments;
-    }
-
-    public byte[] getPicture() {
-        return picture;
-    }
-
-    public void setPicture(byte[] picture) {
-        this.picture = picture;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BlogPost blogPost = (BlogPost) o;
-        return Objects.equals(Id, blogPost.Id) && Objects.equals(user, blogPost.user) && Objects.equals(title, blogPost.title) && Objects.equals(text, blogPost.text) && Objects.equals(comments, blogPost.comments) && Arrays.equals(picture, blogPost.picture) && Objects.equals(date, blogPost.date);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(Id, user, title, text, comments, date);
-        result = 31 * result + Arrays.hashCode(picture);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "BlogPost{" +
-                "Id=" + Id +
-                ", user=" + user +
-                ", title='" + title + '\'' +
-                ", text='" + text + '\'' +
-                ", comments=" + comments +
-                ", picture=" + Arrays.toString(picture) +
-                ", date=" + date +
-                '}';
-    }
 }
