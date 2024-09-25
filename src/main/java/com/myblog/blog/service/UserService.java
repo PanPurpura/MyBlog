5cc5d77fcebdfd2fe696c0e6ea5eac8a6aa916d0
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-09-25 16:49:03 +0200

    Update: changePassword method added

--- src/main/java/com/myblog/blog/service/UserService.java
+++ src/main/java/com/myblog/blog/service/UserService.java
@@ -1,14 +1,16 @@
 package com.myblog.blog.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.myblog.blog.dto.ChangePasswordDto;
 import com.myblog.blog.dto.CredentialsDto;
 import com.myblog.blog.dto.UserDto;
+import com.myblog.blog.exception.InvalidPasswordException;
 import com.myblog.blog.mapper.CredentialsMapper;
 import com.myblog.blog.mapper.UserMapper;
 import com.myblog.blog.model.User;
 import com.myblog.blog.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -21,12 +23,13 @@ public class UserService {
     private final UserRepository repository;
     private final UserMapper userMapper;
     private final CredentialsMapper credentialsMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public List<User> getAll() {
         return (List<User>) repository.findAll();
     }
 
-    public User updateUser(UserDto dto, Principal connectedUser) {
+    public User updateUserInfo(UserDto dto, Principal connectedUser) {
         var myUser = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
         userMapper.updateUserFromDto(dto, myUser);
         repository.save(myUser);
@@ -48,6 +51,23 @@ public class UserService {
         credentialsMapper.updateUserFromCredentialsDto(dto, myUser);
         repository.save(myUser);
         return "Credentials Updated";
+    }
+
+    public void changePassword(ChangePasswordDto request, Principal connectedUser) {
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+            throw new InvalidPasswordException("Wrong password!");
+        }
+
+        if (!request.getNewPassword().equals(request.getConfirmPassword())) {
+            throw new InvalidPasswordException("Passwords are not the same!");
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        repository.save(user);
+
     }
 
 }
