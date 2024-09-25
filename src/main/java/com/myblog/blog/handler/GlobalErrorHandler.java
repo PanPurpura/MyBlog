52fc01fa9b46802b9c71236ea69c5fe645142324
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-09-25 16:08:43 +0200

    Create: Handler class for handling custom errors

---
+++ src/main/java/com/myblog/blog/handler/GlobalErrorHandler.java
@@ -0,0 +1,37 @@
+package com.myblog.blog.handler;
+
+import com.myblog.blog.dto.ErrorDto;
+import com.myblog.blog.exception.NotFoundException;
+import com.myblog.blog.exception.UserExistException;
+import com.myblog.blog.model.User;
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(NOT_FOUND).body(new ErrorDto(NOT_FOUND.value(), NOT_FOUND.getReasonPhrase(), e.getMessage()));
+    }
+
+    @ExceptionHandler(UserExistException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleUserExistException(UserExistException e) {
+        return ResponseEntity.status(BAD_REQUEST).body(new ErrorDto(BAD_REQUEST.value(), BAD_REQUEST.getReasonPhrase(), e.getMessage()));
+    }
+
+    @ExceptionHandler(JwtException.class)
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleJwtException(JwtException e) {
+        return ResponseEntity.status(UNAUTHORIZED).body(new ErrorDto(UNAUTHORIZED.value(), UNAUTHORIZED.getReasonPhrase(), e.getMessage()));
+    }
+
+}
