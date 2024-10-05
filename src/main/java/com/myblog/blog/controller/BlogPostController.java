fe03746a80b7fb74e94e154fca875a76ceb1ceef
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-10-05 13:19:18 +0200

    Create BlogPostController.java

---
+++ src/main/java/com/myblog/blog/controller/BlogPostController.java
@@ -0,0 +1,14 @@
+package com.myblog.blog.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/blog-post")
+public class BlogPostController {
+
+
+
+}
