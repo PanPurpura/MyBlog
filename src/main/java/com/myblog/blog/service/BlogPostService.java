06c2936e9e6ec7e6c8c7216cec3bf92a7171abe2
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-10-05 13:21:54 +0200

    Create BlogPostService.java

---
+++ src/main/java/com/myblog/blog/service/BlogPostService.java
@@ -0,0 +1,12 @@
+package com.myblog.blog.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BlogPostService {
+
+
+
+}
