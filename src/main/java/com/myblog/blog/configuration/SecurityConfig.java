91f417192a530907b9ec63b558f94bdfad064b77
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-10-05 13:19:13 +0200

    Update: New role based authentication added

--- src/main/java/com/myblog/blog/configuration/SecurityConfig.java
+++ src/main/java/com/myblog/blog/configuration/SecurityConfig.java
@@ -1,34 +1,64 @@
 package com.myblog.blog.configuration;
 
-import jakarta.servlet.Filter;
+import com.myblog.blog.handler.CustomAccessDeniedHandler;
+import com.myblog.blog.handler.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import static com.myblog.blog.model.Permission.*;
+import static com.myblog.blog.model.Role.*;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> {
-                    request.requestMatchers("api/v1/auth/**")
-                            .permitAll()
+                    request
+                            .requestMatchers("api/v1/auth/**").permitAll()
+
+                            .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name(), USER.name(), MODERATOR.name())
+
+                            .requestMatchers(HttpMethod.GET, "api/v1/users/**").hasAnyAuthority(ADMIN_READ.getPermission(), MODERATOR_READ.getPermission())
+                            .requestMatchers(HttpMethod.PATCH, "api/v1/users/**").hasAuthority(USER_UPDATE.getPermission())
+                            .requestMatchers(HttpMethod.PUT, "api/v1/users/**").hasAuthority(USER_UPDATE.getPermission())
+                            .requestMatchers(HttpMethod.DELETE, "api/v1/users/**").hasAuthority(USER_DELETE.getPermission())
+
+                            .requestMatchers("/api/v1/user/**").hasAnyRole(ADMIN.name(), USER.name(), MODERATOR.name())
+
+                            .requestMatchers(HttpMethod.GET, "api/v1/profile-images/**").hasAnyAuthority(ADMIN_READ.getPermission(), MODERATOR_READ.getPermission())
+                            .requestMatchers(HttpMethod.PATCH, "api/v1/profile-images/**").hasAuthority(USER_UPDATE.getPermission())
+                            .requestMatchers(HttpMethod.POST, "api/v1/profile-images/**").hasAuthority(USER_WRITE.getPermission())
+                            .requestMatchers(HttpMethod.DELETE, "api/v1/profile-images/**").hasAuthority(USER_DELETE.getPermission())
+
                             .anyRequest()
                             .authenticated();
+                })
+                .exceptionHandling(ex -> {
+                    ex.authenticationEntryPoint(new CustomAuthenticationEntryPoint(handlerExceptionResolver));
+                    ex.accessDeniedHandler(new CustomAccessDeniedHandler(handlerExceptionResolver));
                 })
                 .sessionManagement(request -> {
                     request.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -40,4 +70,5 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 }
