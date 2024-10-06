7b16498c573298c3593c0208c4df7f638e9a044b
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-10-06 10:17:30 +0200

    fix: handle response for empty jwt token

--- src/main/java/com/myblog/blog/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/myblog/blog/configuration/JwtAuthenticationFilter.java
@@ -1,11 +1,13 @@
 package com.myblog.blog.configuration;
 
 import com.myblog.blog.service.JwtService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,15 +16,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver exceptionResolver;
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService_,
+            UserDetailsService userDetailsService_,
+            @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver_
+    ) {
+        this.jwtService = jwtService_;
+        this.userDetailsService = userDetailsService_;
+        this.exceptionResolver = exceptionResolver_;
+
+    }
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -35,25 +49,30 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String userEmail;
         if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
+            exceptionResolver.resolveException(request, response, null, new JwtException("Invalid jwt"));
             return;
         }
+        try {
+            jwt = authenticationHeader.substring(7);
+            userEmail = jwtService.extractUserEmail(jwt);
 
-        jwt = authenticationHeader.substring(7);
-        userEmail = jwtService.extractUserEmail(jwt);
-
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails user = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, user)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        user,
-                        null,
-                        user.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails user = this.userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(jwt, user)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            user.getAuthorities()
+                    );
+                    authToken.setDetails(
+                            new WebAuthenticationDetailsSource().buildDetails(request)
+                    );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+        } catch (JwtException e) {
+            exceptionResolver.resolveException(request, response, null, e);
+            return;
         }
         filterChain.doFilter(request, response);
     }
