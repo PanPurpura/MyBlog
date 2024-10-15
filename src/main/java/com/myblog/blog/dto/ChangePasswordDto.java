d478601f7730397690dead38257c7be34511c1d2
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-10-15 09:09:56 +0200

    feat: added not null constants to the fields.

--- src/main/java/com/myblog/blog/dto/ChangePasswordDto.java
+++ src/main/java/com/myblog/blog/dto/ChangePasswordDto.java
@@ -3,13 +3,18 @@ package com.myblog.blog.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class ChangePasswordDto {
 
+    @NonNull
     private String oldPassword;
+    @NonNull
     private String newPassword;
+    @NonNull
     private String confirmPassword;
 }
